2a527b91d78bdab58ba04914f3a17b98dad84588
A.Shuhouskaya <A.Shuhouskaya@itechart-group.com>
2019-02-21 16:48:18 +0300

    Fixed bug. Separate on the service

--- routes/index.js
+++ routes/index.js
@@ -7,33 +7,32 @@ const APP_ID = "cea0d1b9d40e4364b7ddf70163ff1cbd";
 
 async function saveCurrency(req, res, next) {
   console.log("Express1");
+  // Do you have a request on this day?
   await oxr.set({ app_id: APP_ID });
   await oxr.latest(function(error) {
     if (error) {
       console.log(error.toString());
       return false;
     }
-    console.log("Now: " + new Date(oxr.timestamp).toUTCString());
-    console.log("USD -> EUR: " + oxr.rates.EUR);
-    console.log("USD -> GBP: " + oxr.rates.GBP);
-    fx.rates = oxr.rates;
-    fx.base = oxr.base;
+    // Save this data on database
+    req.now = new Date(oxr.timestamp).toUTCString();
+    req.USD = oxr.rates.USD;
+    req.EUR = oxr.rates.EUR;
+    req.GBP = oxr.rates.GBP;
+    next();
   });
-  next();
-  req.now = new Date(oxr.timestamp).toUTCString();
-  req.EUR = oxr.rates.EUR;
-  req.GBP = oxr.rates.GBP;
 }
 
-router.get("/", saveCurrency, (req, res) => {
+async function currencyService(req, res) {
   console.log("Express2");
-  console.log(`Now: ${req.now} EUR: ${req.EUR} GBP: ${req.GBP} `)
   res.render("index", {
-    title: "Express",
     now: req.now,
-    EUR: req.now,
+    USD: req.USD,
+    EUR: req.EUR,
     GBP: req.GBP
   });
-});
+}
+
+router.get("/", saveCurrency, currencyService);
 
 module.exports = router;
