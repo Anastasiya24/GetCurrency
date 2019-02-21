1358183b0174e5f843b01bda362ad372da7dd8d4
A.Shuhouskaya <A.Shuhouskaya@itechart-group.com>
2019-02-21 16:00:22 +0300

    Added a data

--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,10 @@ var fx = require("money");
 
 const APP_ID = "cea0d1b9d40e4364b7ddf70163ff1cbd";
 
-saveCurrency = (req, res, next) => {
+async function saveCurrency(req, res, next) {
   console.log("Express1");
-  oxr.set({ app_id: APP_ID });
-  // oxr.historical("2019-02-20", function() {
-  //   console.log("USD: ", oxr.rates.USD);
-  //   console.log("EUR: ", oxr.rates.EUR);
-  //   console.log("GBP: ", oxr.rates.GBP);
-  // });
-  oxr.latest(function(error) {
+  await oxr.set({ app_id: APP_ID });
+  await oxr.latest(function(error) {
     if (error) {
       console.log(error.toString());
       return false;
@@ -23,18 +18,22 @@ saveCurrency = (req, res, next) => {
     console.log("USD -> GBP: " + oxr.rates.GBP);
     fx.rates = oxr.rates;
     fx.base = oxr.base;
-    // var amount = fx(10)
-    //   .from("EUR")
-    //   .to("GBP")
-    //   .toFixed(6);
-    // console.log("10 EUR in GBP: " + amount);
   });
   next();
-};
+  req.now = new Date(oxr.timestamp).toUTCString();
+  req.EUR = oxr.rates.EUR;
+  req.GBP = oxr.rates.GBP;
+}
 
 router.get("/", saveCurrency, (req, res) => {
   console.log("Express2");
-  res.render("index", { title: "Express" });
+  console.log(`Now: ${req.now} EUR: ${req.EUR} GBP: ${req.GBP} `)
+  res.render("index", {
+    title: "Express",
+    now: req.now,
+    EUR: req.now,
+    GBP: req.GBP
+  });
 });
 
 module.exports = router;
