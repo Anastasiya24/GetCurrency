e01a9fdb7f161a78816875f10821986286e4c47a
A.Shuhouskaya <A.Shuhouskaya@itechart-group.com>
2019-02-22 15:41:37 +0300

    Added a moment.js

--- routes/index.js
+++ routes/index.js
@@ -1,51 +1,63 @@
 var express = require("express");
 var router = express.Router();
 var oxr = require("open-exchange-rates");
-var fx = require("money");
+var moment = require("moment");
 const Currency = require("../models/Currency");
 const listOfCurrencyPriority = require("../assets/priorityOfCurrencyProviders");
 
 const APP_ID = "cea0d1b9d40e4364b7ddf70163ff1cbd";
+const currentDate = new Date();
 
 saveCurrency = async (req, res, next) => {
-  // Do you have a request on this day? 12pm today
-  await oxr.set({ app_id: APP_ID });
-  await oxr.latest(async function(error) {
-    if (error) {
-      return res.status(404).json({ error });
-    }
-    const USD = oxr.rates.USD;
-    const EUR = oxr.rates.EUR;
-    const GBP = oxr.rates.GBP;
-    const newCurrency = new Currency({
-      provider: "oxr",
-      currencies: {
-        USDEUR: USD / EUR,
-        USDGBP: USD / GBP,
-        EURUSD: EUR / USD,
-        EURGBP: EUR / GBP,
-        GBPUSD: GBP / USD,
-        GBPEUR: GBP / EUR
+  let provider = "oxr";
+  let day = moment(currentDate).format("YYYY-MM-DD");
+  let currentCurrency = await getCurrency(provider, currentDate);
+  if (currentCurrency) next();
+  else {
+    await oxr.set({ app_id: APP_ID });
+    await oxr.historical(day, async function(error) {
+      if (error) {
+        return res.status(404).json({ error });
       }
+      const USD = oxr.rates.USD;
+      const EUR = oxr.rates.EUR;
+      const GBP = oxr.rates.GBP;
+      const newCurrency = new Currency({
+        provider: "oxr",
+        currencies: {
+          USDEUR: USD / EUR,
+          USDGBP: USD / GBP,
+          EURUSD: EUR / USD,
+          EURGBP: EUR / GBP,
+          GBPUSD: GBP / USD,
+          GBPEUR: GBP / EUR
+        }
+      });
+      await newCurrency.save();
     });
-    await newCurrency.save();
-  });
-  next();
+    next();
+  }
 };
 
-getCurrency = async provider => {
-  return await Currency.findOne(
-    { provider: provider },
-    {},
-    { sort: { date: -1 } }
-  );
+getCurrency = async (provider, day) => {
+  // let toDate = moment(day).format("YYYY-MM-DD").subtract(1, 'days');
+  // let laterDate = moment(day).format("YYYY-MM-DD").add(1, 'days');
+  return await Currency.findOne({
+    provider: provider,
+    date: {
+      // $gte: toDate,
+      // $lte: laterDate
+      $gte: "2019-02-21",
+      $lte: "2019-02-23"
+    }
+  });
 };
 
 currencyService = async (req, res) => {
   // Get a currency with a provider's priority
   let provider = "oxr";
-  let currentCurrency = await getCurrency(provider);
-  return res.status(200).render("index", {
+  let currentCurrency = await getCurrency(provider, currentDate);
+  res.status(200).render("index", {
     now: currentCurrency.date,
     provider: currentCurrency.provider,
     oxr_USDEUR: currentCurrency.currencies.USDEUR,
