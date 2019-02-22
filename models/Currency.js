30206ee44a0256ffc409e523edaadd5e2851d427
A.Shuhouskaya <A.Shuhouskaya@itechart-group.com>
2019-02-22 09:13:23 +0300

    22.02 Created a model of currency and save new data in this collection

---
+++ models/Currency.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const CurrencySchema = new Schema({
+  provider: {
+    type: String,
+    required: true
+  },
+  currencies: [
+    {
+      USD: {
+        type: Number,
+        required: true
+      },
+      EUR: {
+        type: Number,
+        required: true
+      },
+      GBP: {
+        type: Number,
+        required: true
+      }
+    }
+  ],
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("currency", CurrencySchema);
--- routes/index.js
+++ routes/index.js
@@ -2,34 +2,41 @@ var express = require("express");
 var router = express.Router();
 var oxr = require("open-exchange-rates");
 var fx = require("money");
+const Currency = require("../models/Currency");
 
 const APP_ID = "cea0d1b9d40e4364b7ddf70163ff1cbd";
 
 async function saveCurrency(req, res, next) {
-  console.log("Express1");
-  // Do you have a request on this day?
+  // Do you have a request on this day? 12pm today
   await oxr.set({ app_id: APP_ID });
-  await oxr.latest(function(error) {
+  await oxr.latest(async function(error) {
     if (error) {
       console.log(error.toString());
       return false;
     }
-    // Save this data on database
     req.now = new Date(oxr.timestamp).toUTCString();
-    req.USD = oxr.rates.USD;
-    req.EUR = oxr.rates.EUR;
-    req.GBP = oxr.rates.GBP;
+    req.provider = "oxr";
+    req.oxr_USD = oxr.rates.USD;
+    req.oxr_EUR = oxr.rates.EUR;
+    req.oxr_GBP = oxr.rates.GBP;
+    const newCurrency = new Currency({
+      provider: "oxr",
+      currencies: [
+        { USD: oxr.rates.USD, EUR: oxr.rates.EUR, GBP: oxr.rates.GBP }
+      ]
+    });
+    await newCurrency.save();
     next();
   });
 }
 
 async function currencyService(req, res) {
-  console.log("Express2");
+  // Get a currency with a provider's priority
   res.render("index", {
     now: req.now,
-    USD: req.USD,
-    EUR: req.EUR,
-    GBP: req.GBP
+    oxr_USD: req.oxr_USD,
+    oxr_EUR: req.oxr_EUR,
+    oxr_GBP: req.oxr_GBP
   });
 }
 
